45e31ea03dab988b8d5d50fee696461c6e2aed19
Moritz Angermann <moritz.angermann@gmail.com>
2020-06-04 17:05:19 +0800

    adds arm targets.

--- release.nix
+++ release.nix
@@ -11,6 +11,15 @@ let nativePkgs = import ./. args;
         "${builtins.currentSystem}" = nativePkgs;
         x86-musl64 = musl64;
         x86-win64 = mingwW64;
+
+        # arm builds for good measure.
+        rpi32-gnu = armv7l-hf-multiplatform;
+        # sadly this one is missing from the nixpkgs system examples
+        rpi32-musl = import nixpkgsSrc (nativePkgs.lib.recursiveUpdate nixpkgsArgs
+            { crossSystem = nativePkgs.lib.systems.examples.armv7l-hf-multiplatform
+                        // { config = "armv7l-unknown-linux-musleabihf"; }; });
+        rpi64-gnu = aarch64-multiplatform;
+        rpi64-musl = aarch64-multiplatform-musl;
     };
 in with nativePkgs.lib;
 flip mapAttrs targets (_: pkgs:
