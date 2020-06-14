ae81f2061ae3ed2abdbd044fd5355f7796ce192f
Moritz Angermann <moritz.angermann@gmail.com>
2020-06-14 12:16:20 +0000

    Add macOS targets

--- default.nix
+++ default.nix
@@ -1,12 +1,15 @@
 { sources ? import ./nix/sources.nix { }
 , nixpkgs ? sources.nixpkgs
 , sourcesOverride ? {}
+, system ? builtins.currentSystem
 }:
 let
   sources' = sources // sourcesOverride;
 in
 # Add rust packages to nixpkgs via the overlay in overlay.nix
-import nixpkgs { overlays = [
+import nixpkgs {
+  inherit system;
+  overlays = [
     (import sources'."rust.nix")
     (import ./overlay.nix)
     # For debugging with a local rust.nix checkout.
--- release.nix
+++ release.nix
@@ -6,10 +6,14 @@
 # The idea is to have rustPkgs listed (or we'd somehow have to extract the names
 # somehow out of the overlay.nix). And the targets we want, and then just
 # produce the products (rustPkgs x targets) we want hydra to build.
-let nativePkgs = import ./. args;
+let linuxPkgs = import ./. (args  // { system = "x86_64-linux"; });
+    macosPkgs  = import ./. (args // { system = "x86_64-darwin"; });
+    nativePkgs = import ./. args;
     rustPkgs = [ "kes_mmm_sumed25519_c" "rust-test" "vit-servicing-station" ];
-    targets = with nativePkgs.pkgsCross; {
-        "${builtins.currentSystem}" = nativePkgs;
+    # We cross compile only from linux.
+    targets = with linuxPkgs.pkgsCross; {
+        "x86_64-linux" = linuxPkgs;
+        "x86_64-macos" = macosPkgs;
         x86-musl64 = musl64;
         x86-win64 = mingwW64;
 
@@ -18,9 +22,9 @@ let nativePkgs = import ./. args;
         # sadly this one is missing from the nixpkgs system examples
         # This is a mess, we have no way to access nixpkgsFun or the
         # original arguments nixpkgs was called with.
-        rpi32-musl = import nativePkgs.path {
-            inherit (nativePkgs) overlays config;
-            crossSystem = nativePkgs.lib.systems.examples.armv7l-hf-multiplatform
+        rpi32-musl = import linuxPkgs.path {
+            inherit (linuxPkgs) overlays config;
+            crossSystem = linuxPkgs.lib.systems.examples.armv7l-hf-multiplatform
               // { config = "armv7l-unknown-linux-musleabihf"; };
             };
         rpi64-gnu = aarch64-multiplatform;
